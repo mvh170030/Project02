6514c46014969e3f4cce1f4f42e6d1b2bb7db566
David Hernandez <mvh170030@utdallas.edu>
2020-10-02 20:41:16 -0500

    Game State

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioClip _startingSong;
+    public static AudioManager Instance = null;
+    AudioSource _audioSource;
+
+    void Start()
+    {
+        // play starting song on menu start
+        if (_startingSong != null)
+        {
+            AudioManager.Instance.PlaySong(_startingSong);
+        }
+    }
+
+    private void Awake()
+    {
+        #region Singleton Pattern (Simple)
+        if (Instance == null)
+        {
+            // doesnt exist yet
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            // fill references
+            _audioSource = GetComponent<AudioSource>();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        #endregion
+    }
+
+    public void PlaySong(AudioClip clip)
+    {
+        _audioSource.clip = clip;
+        _audioSource.Play();
+    }
+}
---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    
+    [SerializeField] Text _highScoreTextView;
+
+    void Start()
+    {
+        // load hs score display
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        _highScoreTextView.text = highScore.ToString();
+    }
+
+    public void ResetData()
+    {
+        PlayerPrefs.DeleteAll();
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        _highScoreTextView.text = highScore.ToString();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
