cb6e349859c49b4df037c7a41d062c2abf973e67
David Hernandez <mvh170030@utdallas.edu>
2020-10-02 20:41:16 -0500

    Player Controller

---
+++ Assets/Scripts/Level01Controller.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Level01Controller : MonoBehaviour
+{
+    [SerializeField] Text _currentScoreTextView;
+
+    int _currentScore;
+    public GameObject pauseMenu;
+
+    private void Update()
+    {
+        // increase score
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            IncreaseScore(5);
+        }
+        // pause
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.SetActive(!pauseMenu.activeSelf);
+        }
+    }
+
+    public void IncreaseScore(int scoreIncrease)
+    {
+        // increase score
+        _currentScore += scoreIncrease;
+        // update score display
+        _currentScoreTextView.text =
+            "Score: " + _currentScore.ToString();
+    }
+
+    public void ExitLevel()
+    {
+        // compare score to highscore
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        if (_currentScore > highScore)
+        {
+            // save current score as new highscore
+            PlayerPrefs.SetInt("HighScore", _currentScore);
+            Debug.Log("New high score: " + _currentScore);
+        }
+        // load new level
+        SceneManager.LoadScene("MainMenu");
+    }
+}
